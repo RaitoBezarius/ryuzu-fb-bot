fc70feec85f0b22ebef05b0fa1316214a48a465a
Raito Bezarius <masterancpp@gmail.com>
2017-07-05 17:33:47 +0200

    Use REDIS_URL by default for Celery

--- background/config/prod.py
+++ background/config/prod.py
@@ -2,8 +2,12 @@ from decouple import config
 
 from .base import BaseCeleryConfig
 
+REDIS_URL = config('REDIS_URL')
+
 
 class CeleryProduction(BaseCeleryConfig):
     enable_utc = config('CELERY_ENABLE_UTC', default=True, cast=bool)
-    broker_url = config('CELERY_BROKER_URL')
-    result_backend = config('CELERY_RESULT_BACKEND')
+    broker_url = config('CELERY_BROKER_URL',
+                        default=REDIS_URL)
+    result_backend = config('CELERY_RESULT_BACKEND',
+                            default=REDIS_URL)
